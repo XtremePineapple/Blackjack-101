1b0a817f6b84527735bbb453788aab0361a39dfa
rsweaver <weavaweave735@gmail.com>
2019-03-23 11:05:34 -0700

    merge struggles

--- gameLogic.js
+++ gameLogic.js
@@ -1,6 +1,10 @@
 let playerHand = [];
 let dealerHand = [];
 let count = 0;
+let hitButton = document.getElementById("hit");
+let dealButton = document.getElementById("deal");
+let stayButton = document.getElementById("stay");
+let quitButton = document.getElementById("quit");
 
 function getDeck(){
     
@@ -8,4 +12,3 @@ function getDeck(){
 
 
 
-// 
